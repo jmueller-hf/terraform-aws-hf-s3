b0c8ce795e6c97d02885b89b4cbcb5ab747ef7d2
jmueller-hf <122904141+jmueller-hf@users.noreply.github.com>
2023-03-08 11:26:37 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,8 @@
 variable "bucket" {
   type        = string
   description = "S3 Bucket"
+  validation {
+    condition = can(regex("^hf-[a-z0-9-]*", lower(var.bucket)))
+    error_message = "Bucket name must be start with hf- and contain only alphanumeric characters [a-z0-9-]"
+  }
 }
