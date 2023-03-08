7a0af7b86e8420c6a09572cac931c39df2f9af29
jmueller-hf <122904141+jmueller-hf@users.noreply.github.com>
2023-03-08 11:15:21 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,21 +2,3 @@ variable "bucket" {
   type        = string
   description = "S3 Bucket"
 }
-
-variable "versioning" {
-  type        = string
-  description = "Bucket Versioning"
-  default     = "Disabled"
-}
-
-variable "sse_algorithm" {
-  type        = string
-  description = "Bucket Versioning"
-  default     = null
-}
-
-variable "kms_master_key_id" {
-  type        = string
-  description = "Bucket Versioning"
-  default     = null
-}
