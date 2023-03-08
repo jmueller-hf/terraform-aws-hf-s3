270dcd117c9b9973bc1580a172db07d5950d5859
jmueller-hf <122904141+jmueller-hf@users.noreply.github.com>
2023-03-08 11:14:22 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,22 +14,3 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.bucket.id
-  versioning_configuration {
-    status = var.versioning
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  count = var.sse_algorithm != null ? 1 : 0
-  bucket = aws_s3_bucket.bucket.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = var.sse_algorithm
-      kms_master_key_id = var.kms_master_key_id
-    }
-  }
-}
