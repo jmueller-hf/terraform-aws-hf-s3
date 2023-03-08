72769413aac8283459fc21969e02fe055f10f2b4
jmueller-hf <122904141+jmueller-hf@users.noreply.github.com>
2023-03-08 11:32:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,51 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  count = length(compact(data.aws_iam_policy_document.combined_policy.source_policy_documents))
+  bucket = var.bucket
+  policy = data.aws_iam_policy_document.combined_policy.json
+}
+
+data "aws_iam_policy_document" "force_ssl_only" {
+  statement {
+    effect = "Deny"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions = ["s3:*"]
+    resources = ["arn:aws:s3:::${var.bucket}/*"]
+    condition {
+      test = "Bool"
+      variable = "aws:SecureTransport"
+      values = ["false"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "deny_unencrypted_object_upload" {
+  statement {
+    effect = "Deny"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${var.bucket}/*"]
+    condition {
+      test = "Null"
+      variable = "s3:x-amz-server-side-encryption"
+      values = ["true"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "combined_policy" {
+  source_policy_documents = compact([
+    data.aws_iam_policy_document.force_ssl_only.json,
+    data.aws_iam_policy_document.deny_unencrypted_object_upload.json,
+    (data.aws_iam_policy_document.combined_policy_access.source_policy_documents != null ? data.aws_iam_policy_document.combined_policy_access.json : ""),
+  ])
+}
